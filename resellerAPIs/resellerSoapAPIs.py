39b921dcfeea63f58eb2835706bc6d6130fc545d
Debaditya M <Debaditya_Mohankudo@symantec.com>
2016-11-26 12:44:26 +0530

    minor

--- resellerAPIs/resellerSoapAPIs.py
+++ resellerAPIs/resellerSoapAPIs.py
@@ -209,22 +209,3 @@ class Util():
             print (k, '-->', v)
 
 
-#for unit testing the functions
-if __name__ == '__main__':
-    wsdl = 'file:///' +  os.path.abspath("./query.jws.xml")
-    #url = 'https://api.ws.symantec.com/webtrust/query.jws?WSDL'
-    h = TestApi(wsdl,
-        'GetOrderByPartnerOrderID',
-        authToken_UserName='username',
-        password='password',
-        partnercode='partnercode',
-        PartnerOrderID='parterorderid',
-        ReturnProductDetail=True,
-        ReturnContacts=True,
-        criterion = [1,2])
-    h.process_soap_request()
-    h.validate_and_capture_response_data(
-        returncount=0,
-        queryresponseheader_successcode=0)
-    h.print_relevant_data()
-
